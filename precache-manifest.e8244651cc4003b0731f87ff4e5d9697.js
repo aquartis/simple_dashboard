01e94ec91fc2d675b140fd08c17f571a30e9013f
Eugene Worker <1zheka92@gmail.com>
2019-07-29 13:28:55 +0300

    Updates

---
+++ precache-manifest.e8244651cc4003b0731f87ff4e5d9697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5302493a36bc09a09e763f2ac684f06",
+    "url": "/simple_dasboard/index.html"
+  },
+  {
+    "revision": "dd7a2ae46823af8a5e7d",
+    "url": "/simple_dasboard/static/css/2.176df4b7.chunk.css"
+  },
+  {
+    "revision": "c98abce8e60667ea9952",
+    "url": "/simple_dasboard/static/css/main.14581ed2.chunk.css"
+  },
+  {
+    "revision": "dd7a2ae46823af8a5e7d",
+    "url": "/simple_dasboard/static/js/2.f140c595.chunk.js"
+  },
+  {
+    "revision": "c98abce8e60667ea9952",
+    "url": "/simple_dasboard/static/js/main.82f00644.chunk.js"
+  },
+  {
+    "revision": "fee4e4ae40c8530d1067",
+    "url": "/simple_dasboard/static/js/runtime~main.48802c87.js"
+  }
+]);
